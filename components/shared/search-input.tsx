9329ff847b9b96d8a1c701c6e927636b0de9ac7a
Alexei Krivchikov <KrivchikovAlexei@yandex.ru>
2025-02-05 17:56:56 +0300

    fixed clear search value

--- components/shared/search-input.tsx
+++ components/shared/search-input.tsx
@@ -55,6 +55,7 @@ export const SearchInput: React.FC<Props> = ({ className }) => {
           className="rounded-2xl outline-none w-full bg-gray-100 pl-11"
           type="text"
           placeholder="Найти пиццу..."
+          value={searchQuery}
           onFocus={() => setFocused(true)}
           onChange={(e) => setSearchQuery(e.target.value)}
         />
