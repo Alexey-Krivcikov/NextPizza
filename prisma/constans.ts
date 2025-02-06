a688572d4635df790d7bbb901f9c396406262a1c
Alexei Krivchikov <KrivchikovAlexei@yandex.ru>
2025-02-06 10:36:03 +0300

    Moved images into project

--- prisma/constans.ts
+++ prisma/constans.ts
@@ -21,178 +21,178 @@ export const ingredients = [
     name: 'Сырный бортик',
     price: 179,
     imageUrl:
-      'https://cdn.dodostatic.net/static/Img/Ingredients/99f5cb91225b4875bd06a26d2e842106.png',
+      '/images/cheese-bort.png',
   },
   {
     name: 'Сливочная моцарелла',
     price: 79,
     imageUrl:
-      'https://cdn.dodostatic.net/static/Img/Ingredients/cdea869ef287426386ed634e6099a5ba.png',
+      '/images/mozarella.png',
   },
   {
     name: 'Сыры чеддер и пармезан',
     price: 79,
-    imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA69C1FE796',
+    imageUrl: '/images/chedder-parmesan.png',
   },
   {
     name: 'Острый перец халапеньо',
     price: 59,
     imageUrl:
-      'https://cdn.dodostatic.net/static/Img/Ingredients/11ee95b6bfdf98fb88a113db92d7b3df.png',
+      '/images/halapeniu.png',
   },
   {
     name: 'Нежный цыпленок',
     price: 79,
-    imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A39D824A82E11E9AFA5B328D35A',
+    imageUrl: '/images/chiken.png',
   },
   {
     name: 'Шампиньоны',
     price: 59,
-    imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA67259A324',
+    imageUrl: '/images/shampinon.png',
   },
   {
     name: 'Ветчина',
     price: 79,
-    imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A39D824A82E11E9AFA61B9A8D61',
+    imageUrl: '/images/vetchina.png',
   },
   {
     name: 'Пикантная пепперони',
     price: 79,
-    imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA6258199C3',
+    imageUrl: '/images/pepperoni.png',
   },
   {
     name: 'Острая чоризо',
     price: 79,
-    imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA62D5D6027',
+    imageUrl: '/images/chorizo.png',
   },
   {
     name: 'Маринованные огурчики',
     price: 59,
-    imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A21DA51A81211E9EA89958D782B',
+    imageUrl: '/images/pichiles.png',
   },
   {
     name: 'Свежие томаты',
     price: 59,
-    imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A39D824A82E11E9AFA7AC1A1D67',
+    imageUrl: '/images/tomato.png',
   },
   {
     name: 'Красный лук',
     price: 59,
-    imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA60AE6464C',
+    imageUrl: '/images/onion.png',
   },
   {
     name: 'Сочные ананасы',
     price: 59,
-    imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A21DA51A81211E9AFA6795BA2A0',
+    imageUrl: '/images/pinneapple.png',
   },
   {
     name: 'Итальянские травы',
     price: 39,
     imageUrl:
-      'https://cdn.dodostatic.net/static/Img/Ingredients/370dac9ed21e4bffaf9bc2618d258734.png',
+      '/images/italian.png',
   },
   {
     name: 'Сладкий перец',
     price: 59,
-    imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA63F774C1B',
+    imageUrl: '/images/pepper.png',
   },
   {
     name: 'Кубики брынзы',
     price: 79,
-    imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A39D824A82E11E9AFA6B0FFC349',
+    imageUrl: '/images/brinza.png',
   },
   {
     name: 'Митболы',
     price: 79,
     imageUrl:
-      'https://cdn.dodostatic.net/static/Img/Ingredients/b2f3a5d5afe44516a93cfc0d2ee60088.png',
+      '/images/meetboll.png',
   },
 ].map((obj, index) => ({ id: index + 1, ...obj }));
 
 export const products = [
   {
     name: 'Омлет с ветчиной и грибами',
-    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7970321044479C1D1085457A36EB.webp',
+    imageUrl: '/images/omlet1.webp',
     categoryId: 2,
   },
   {
     name: 'Омлет с пепперони',
-    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE94ECF33B0C46BA410DEC1B1DD6F8.webp',
+    imageUrl: '/images/omlet2.webp',
     categoryId: 2,
   },
   {
     name: 'Кофе Латте',
-    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D61B0C26A3F85D97A78FEEE00AD.webp',
+    imageUrl: '/images/latte1.webp',
     categoryId: 2,
   },
   {
     name: 'Дэнвич ветчина и сыр',
-    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE796FF0059B799A17F57A9E64C725.webp',
+    imageUrl: '/images/denwitch1.webp',
     categoryId: 3,
   },
   {
     name: 'Куриные наггетсы',
-    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D618B5C7EC29350069AE9532C6E.webp',
+    imageUrl: '/images/nuggets.webp',
     categoryId: 3,
   },
   {
     name: 'Картофель из печи с соусом 🌱',
-    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EED646A9CD324C962C6BEA78124F19.webp',
+    imageUrl: '/images/potato1.webp',
     categoryId: 3,
   },
   {
     name: 'Додстер',
-    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE796F96D11392A2F6DD73599921B9.webp',
+    imageUrl: '/images/dodster.webp',
     categoryId: 3,
   },
   {
     name: 'Острый Додстер 🌶️🌶️',
-    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE796FD3B594068F7A752DF8161D04.webp',
+    imageUrl: '/images/dodster2.webp',
     categoryId: 3,
   },
   {
     name: 'Банановый молочный коктейль',
-    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EEE20B8772A72A9B60CFB20012C185.webp',
+    imageUrl: '/images/banana-coctail.webp',
     categoryId: 4,
   },
   {
     name: 'Карамельное яблоко молочный коктейль',
-    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE79702E2A22E693D96133906FB1B8.webp',
+    imageUrl: '/images/apple-cocktail.webp',
     categoryId: 4,
   },
   {
     name: 'Молочный коктейль с печеньем Орео',
-    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE796FA1F50F8F8111A399E4C1A1E3.webp',
+    imageUrl: '/images/oreo-coctail.webp',
     categoryId: 4,
   },
   {
     name: 'Классический молочный коктейль 👶',
-    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE796F93FB126693F96CB1D3E403FB.webp',
+    imageUrl: '/images/cocktail.webp',
     categoryId: 4,
   },
   {
     name: 'Ирландский Капучино',
-    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D61999EBDA59C10E216430A6093.webp',
+    imageUrl: '/images/irish-coffe.webp',
     categoryId: 5,
   },
   {
     name: 'Кофе Карамельный капучино',
-    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D61AED6B6D4BFDAD4E58D76CF56.webp',
+    imageUrl: '/images/caram-coffe.webp',
     categoryId: 5,
   },
   {
     name: 'Кофе Кокосовый латте',
-    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D61B19FA07090EE88B0ED347F42.webp',
+    imageUrl: '/images/cocos-latte.webp',
     categoryId: 5,
   },
   {
     name: 'Кофе Американо',
-    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D61B044583596548A59078BBD33.webp',
+    imageUrl: '/images/amerikano.webp',
     categoryId: 5,
   },
   {
     name: 'Кофе Латте',
-    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D61B0C26A3F85D97A78FEEE00AD.webp',
+    imageUrl: '/images/latte.webp',
     categoryId: 5,
   },
 ];
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -59,7 +59,7 @@ async function up() {
   const pizza1 = await prisma.product.create({
     data: {
       name: "Пепперони фреш",
-      imageUrl: "/pepper.webp",
+      imageUrl: "/images/pepper.webp",
       categoryId: 1,
       ingredients: {
         connect: ingredients.slice(0, 5),
@@ -70,7 +70,7 @@ async function up() {
   const pizza2 = await prisma.product.create({
     data: {
       name: "Сырная",
-      imageUrl: "/cheese.webp",
+      imageUrl: "/images/cheese.webp",
       categoryId: 1,
       ingredients: {
         connect: ingredients.slice(5, 10),
@@ -81,7 +81,7 @@ async function up() {
   const pizza3 = await prisma.product.create({
     data: {
       name: "Чоризо фреш",
-      imageUrl: "/chorizo.webp",
+      imageUrl: "/images/chorizo.webp",
       categoryId: 1,
       ingredients: {
         connect: ingredients.slice(10, 40),
