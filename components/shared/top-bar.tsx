236bc0367851fb919851edbbaccb2d1d1567dbaf
Alexei Krivchikov <KrivchikovAlexei@yandex.ru>
2025-02-07 16:58:54 +0300

    Added slot for modal product popup

--- components/shared/top-bar.tsx
+++ components/shared/top-bar.tsx
@@ -1,16 +1,18 @@
 import { cn } from '@/lib/utils';
 import React from 'react';
 import { Categories, Container, SortPopup } from '.';
+import { Category } from '@prisma/client';
 
 interface Props {
   className?: string;
+  categories: Category[]
 }
 
-export const TopBar: React.FC<Props> = ({ className }) => {
+export const TopBar: React.FC<Props> = ({ className, categories }) => {
   return (
     <div className={cn('sticky top-0 bg-white py-5 shadow-lg shadow-black/5 z-10', className)}>
       <Container className="flex items-center justify-between ">
-        <Categories />
+        <Categories items={categories} />
         <SortPopup />
       </Container>
     </div>
