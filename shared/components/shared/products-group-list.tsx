343d8251e6c7bd69f91c35f819b15bfda5356053
Alexei Krivchikov <KrivchikovAlexei@yandex.ru>
2025-02-24 17:46:43 +0300

    added filter pizzas logic

--- shared/components/shared/products-group-list.tsx
+++ shared/components/shared/products-group-list.tsx
@@ -5,10 +5,11 @@ import { useIntersection } from 'react-use';
 import { ProductCard, Title } from '.';
 import { cn } from '@/shared/lib/utils';
 import { useCategoryStore } from '@/shared/store/category';
+import { ProductWithRelations } from '@/@types/prisma';
 
 interface Props {
   title: string;
-  items: any[];
+  items: ProductWithRelations[];
   categoryId: number;
   className?: string;
   listClassName?: string;
@@ -40,6 +41,7 @@ export const ProductsGroupList: React.FC<Props> = ({ title, items, listClassName
             name={product.name}
             imageUrl={product.imageUrl}
             price={product.items[0].price}
+            ingredients={product.ingredients}
           />
         ))}
       </div>
